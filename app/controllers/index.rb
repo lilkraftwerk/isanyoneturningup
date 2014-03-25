a04962f529995f33e48cef704b4683c2cefd43ff
lilkraftwerk <lucasquinnmathis@gmail.com>
2014-03-25 13:35:24 -0700

    places tweets on the map

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,3 +1,22 @@
 get '/' do
   erb :index
 end
+
+get '/getmore' do
+  client = make_twitter
+  client.search("turnup").take(200).collect do |tweet|
+    if tweet.geo?
+      Tweet.create(
+        tweet_id: tweet.id,
+        coords: tweet.geo.coords,
+        content: client.oembed(tweet.id).html,
+        lat: tweet.geo.lat,
+        long: tweet.geo.long
+        )
+  end
+end
+end
+
+get '/tweet' do
+  Tweet.first(:order => "RANDOM()").to_json
+end
