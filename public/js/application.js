562bdcae2fb5da8b5cb642452a13a4addecf7aaf
lilkraftwerk <lucasquinnmathis@gmail.com>
2014-03-25 18:13:58 -0700

    javascript loop working

--- public/js/application.js
+++ public/js/application.js
@@ -1,13 +1,20 @@
 $(document).ready(function() {
+
+
     $(document).on('click', '#turnup', function() {
         getTweet();
+    })
+
+    $(document).on('click', '#turnloop', function() {
+        getTweet();
+        turntLoop();
     })
 });
 
 function initialize() {
     var mapOptions = {
-        center: new google.maps.LatLng(-34.397, 150.644),
-        zoom: 4
+        center: new google.maps.LatLng(37.77056, -122.42694),
+        zoom: 3
     };
     map = new google.maps.Map(document.getElementById("map-canvas"),
         mapOptions);
@@ -46,9 +53,31 @@ function getTweet() {
         type: 'get',
         datatype: 'json'
     }).done(function(tweet) {
-        var jaySon = JSON.parse(tweet);
-        var myCatlng = new google.maps.LatLng(parseFloat(jaySon.tweet.lat), parseFloat(jaySon.tweet.long));
-        var newMarker = placeMarker(myCatlng, "hi");
-        placeTurnup(newMarker, jaySon.tweet.content);
+        doTweet(tweet)
     })
 };
+
+function doTweet(tweet) {
+    var jaySon = JSON.parse(tweet);
+    var myCatlng = new google.maps.LatLng(parseFloat(jaySon.tweet.lat), parseFloat(jaySon.tweet.long));
+    var newMarker = placeMarker(myCatlng, "hi");
+    placeTurnup(newMarker, jaySon.tweet.content);
+}
+
+
+
+looping = null;
+
+function turntLoop() {
+    if (!looping) {
+        $('#turnloop').html("cancel loop");
+
+        looping = setInterval(function() {
+            getTweet()
+        }, 4000);
+    } else {
+        $('#turnloop').html("start loop");
+        window.clearInterval(looping);
+        looping = null;
+    }
+}
