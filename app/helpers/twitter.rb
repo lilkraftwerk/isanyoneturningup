e266dfcdd06c8027853160e4ff40a827c62d5db8
lilkraftwerk <lucasquinnmathis@gmail.com>
2014-03-26 17:31:18 -0700

    fixing mistake with api keys being public and such

--- app/helpers/twitter.rb
+++ app/helpers/twitter.rb
@@ -1,10 +1,10 @@
 helpers do
   def make_twitter
     client = Twitter::REST::Client.new do |config|
-      config.consumer_key    = CONFIG["twitter"]["consumer"]
-      config.consumer_secret = CONFIG["twitter"]["consumer_secret"]
-      config.access_token        = CONFIG["twitter"]["access_token"]
-      config.access_token_secret = CONFIG["twitter"]["access_secret"]
+      config.consumer_key    = CONFIG["TWITTER_KEY"]
+      config.consumer_secret = CONFIG["TWITTER_SECRET"]
+      config.access_token        = CONFIG["TOKEN"]
+      config.access_token_secret = CONFIG["TOKEN_SECRET"]
       client
     end
   end
